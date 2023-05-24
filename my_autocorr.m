eea154d6206d7127c7e9a162036bac2a7938dc43
zeroleaked <zirlysukarno@gmail.com>
2023-05-24 17:28:33 +0700

    circular autocorrelation

--- my_autocorr.m
+++ my_autocorr.m
@@ -13,22 +13,8 @@ r = zeros(1, 2*maxlag+1);
 
 % Menggunakan for loop untuk menghitung autokorelasi
 for k = 1:2*maxlag+1
-        % korelasi beberapa lag pertama
-    if lags(k) < 0
-        for i = 1:N+lags(k),
-            r(k) = r(k) + x(i) * x(-lags(k)+i);
-        end
-        % korelasi beberapa lag terakhir
-    elseif lags(k) > 0
-        for i = 1:N-lags(k),
-            r(k) =  r(k) + x(i) * x(lags(k)+i);
-        end
-        % r(k) = sum(x(1:N-lags(k)) .* x(lags(k)+1:N));
-        % korelasi saat kedua sinyal overlap
-    else
-        for i = 1:N,
-            r(k) = r(k) + x(i) * x(i);
-        end
+    for i=1:N
+        r(k) = r(k) + x(i) * x(mod(i+lags(k), N));
     end
 end
 % Normalisasi nilainya
