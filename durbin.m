f7caf6ce65c4c8586772a7f96a10365555155f06
zir <zirlysukarno@gmail.com>
2023-06-21 10:33:06 +0700

    unary

--- durbin.m
+++ durbin.m
@@ -6,6 +6,8 @@ function a = durbin(r0, r, m)
 
 % INITIALIZATION
 a = zeros(m+1,1);
+k = zeros(m+1,1);
+tmp = zeros(m-1,1);
 a(1) = 1;
 E = r0;
 a(2) = -r(1) / r0;
@@ -16,9 +18,20 @@ q = r(1);
 for i = 1:m-1
     E = E + q * k(i);
     q = r(i+1);
-    q = q + sum( r(1:i) .* a(i+1:-1:2));
+    % q = q + sum( r(1:i) .* a(i+1:-1:2));
+    for j = 1:i
+        q = q + r(j) * a(i+2-j);
+    end
     k(i+1) = -q / E;
-    tmp(1:i) = k(i+1) .* a(i+1:-1:2);
-    a(2:i+1) = a(2:i+1) + tmp(1:i)';
+    % tmp(1:i) = k(i+1) .* a(i+1:-1:2);
+    for j = 1:i
+        tmp(j) = k(i+1) * a(i+2-j);
+    end
+    % a(2:i+1) = a(2:i+1) + tmp(1:i)';
+    for j = 1:i
+        a(j+1) = a(j+1) + tmp(j);
+    end
     a(i+2) = k(i+1);
+end
+
 end
--- main.m
+++ main.m
@@ -9,7 +9,7 @@ speechSignal = mean(speechSignal, 2);
 speechSignal = resample(speechSignal, samplingFrequency, Fs);
 Fs = samplingFrequency;
 
-%speechSignal = speechSignal(15361:15680);
+speechSignal = speechSignal(15521:15680);
 
 % Menghitung jumlah frame
 numFrames = ceil(length(speechSignal)/frameLength);
